2b4909875efa4e006f3272ccc7e325b1528b5123
Kemal Arifovic <gigalab@gmail.com>
2021-03-16 19:58:02 +0100

    While Loops

---
+++ ex13/src/index.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+function inverseWhile() {
+  var fiveNumbers = "";
+  var i = 5;
+  while (i >= 0) {
+    fiveNumbers += i;
+    if (i > 0) {
+      fiveNumbers += ",";
+    }
+    i--;
+  }
+  return fiveNumbers;
+}
+
+inverseWhile();
+// Only change code above this line
+
+console.log(inverseWhile());
+module.exports = inverseWhile;
